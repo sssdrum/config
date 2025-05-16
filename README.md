2eff65e3bbb69254b411023571b0623212ebe05a
Luigi Di Paolo <luigi.dipaolo02@gmail.com>
2025-05-16 21:37:13 +0100

    Enable prettier for markdown

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Most of my configs
 
-More info [here](https://medium.com/@victor.cui1996/how-to-store-configuration-files-in-git-e5924340c6ee):
+More info [here](https://medium.com/@victor.cui1996/how-to-store-configuration-files-in-git-e5924340c6ee)
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -16,7 +16,7 @@ return {
         -- html = { "prettier" },
         -- json = { "prettier" },
         -- yaml = { "prettier" },
-        -- markdown = { "prettier" },
+        markdown = { "prettier" },
         -- graphql = { "prettier" },
         lua = { "stylua" }, -- config file: ~/.stylua.toml
         sh = { "shfmt" },
