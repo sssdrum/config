2959989b33f5f2b0bffe17a1a19867f53d7d8f1e
Luigi Di Paolo <luigi.dipaolo02@gmail.com>
2025-05-17 18:50:10 +0100

    Add autocompletion

--- nvim/lua/lsp/bashls.lua
+++ nvim/lua/lsp/bashls.lua
@@ -1,7 +1,10 @@
 local common = require("lsp.common")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 return {
   cmd = { "bash-language-server", "start" },
   filetypes = { "bash", "sh" },
   on_attach = common.on_attach,
+  capabilities = capabilities,
 }
--- nvim/lua/lsp/luals.lua
+++ nvim/lua/lsp/luals.lua
@@ -1,10 +1,13 @@
 local common = require("lsp.common")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 return {
   cmd = { "lua-language-server" },
   filetypes = { "lua" },
   root_markers = { ".luarc.json", ".luarc.jsonc" },
   on_attach = common.on_attach, -- Add this line
+  capabilities = capabilities,
   settings = {
     Lua = {
       diagnostics = {
