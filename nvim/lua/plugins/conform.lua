7b3e106bc7171b349643ebc15645c2001e87053a
Luigi Di Paolo <luigi.dipaolo02@gmail.com>
2025-05-17 17:06:53 +0100

    Enable prettier

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,17 +7,15 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        -- javascript = { "prettier" },
-        -- typescript = { "prettier" },
-        -- javascriptreact = { "prettier" },
-        -- typescriptreact = { "prettier" },
-        -- svelte = { "prettier" },
-        -- css = { "prettier" },
-        -- html = { "prettier" },
-        -- json = { "prettier" },
-        -- yaml = { "prettier" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
         markdown = { "prettier" },
-        -- graphql = { "prettier" },
         lua = { "stylua" }, -- config file: ~/.stylua.toml
         sh = { "shfmt" },
         -- python = { "isort", "black" },
