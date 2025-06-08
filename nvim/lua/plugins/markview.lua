94199c7080ccd90e6c643c913c5d561f30c0a698
Luigi Di Paolo <luigi.dipaolo02@gmail.com>
2025-06-08 17:03:34 +0100

    Disable markview preview in current line

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -1,10 +1,10 @@
 return {
   "OXY2DEV/markview.nvim",
   lazy = false,
-
-  -- For blink.cmp's completion
-  -- source
-  -- dependencies = {
-  --   "saghen/blink.cmp",
-  -- },
+  opts = {
+    preview = {
+      hybrid_modes = { "n" }, -- Enable hybrid mode in normal mode
+      -- You can add other modes like "i" (insert), "v" (visual), etc.
+    },
+  },
 }
